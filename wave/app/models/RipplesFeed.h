02d1ee7a0a978752dff397c29507ca681b864576
Simen Lie <simenlie@roger.local>
2015-06-16 18:51:17 +0200

    #1 Added ripplesfeed model, and added support for getting all ripples

---
+++ wave/app/models/RipplesFeed.h
@@ -0,0 +1,13 @@
+//
+//  RipplesFeed.h
+//  wave
+//
+//  Created by Simen Lie on 16.06.15.
+//  Copyright (c) 2015 ddev. All rights reserved.
+//
+
+#import "SuperModel.h"
+
+@interface RipplesFeed : SuperModel
+
+@end
---
+++ wave/app/models/RipplesFeed.m
@@ -0,0 +1,13 @@
+//
+//  RipplesFeed.m
+//  wave
+//
+//  Created by Simen Lie on 16.06.15.
+//  Copyright (c) 2015 ddev. All rights reserved.
+//
+
+#import "RipplesFeed.h"
+
+@implementation RipplesFeed
+
+@end
